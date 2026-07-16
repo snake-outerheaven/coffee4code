19742a8c89f5d85039a33704ef658ce13425f111
neo-dev <sei133dirsoa@gmail.com>
2026-07-15 21:46:39 -0300

    add more code examples from the Literals and closer look on variables (revise this part)

---
+++ chapter2/examples/DynInit.java
@@ -0,0 +1,11 @@
+package chapter2.examples;
+
+public class DynInit {
+        public static void main(String[] args) {
+                double radius = 4, height = 5;
+
+                double volume = Math.PI * radius * height;
+
+                System.out.println("Volume is " + volume);
+        }
+}
